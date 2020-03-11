52a8c889677a94fdd84a87785d8ba663d1a345a9
Tisdale511 <tisdalefry@gmail.com>
2020-03-11 16:58:57 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_accessor :roster, :name
+
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        
+        if !@roster[grade]
+            
+            @roster[grade] = [name]
+        else
+            @roster[grade] << name   
+        end
+    end
+
+    def sort
+        new_hash = {}
+        @roster.each do |key, value|
+            new_hash[key] = value.sort
+            
+        end
+        new_hash
+    end
+
+    def grade(num)
+        return @roster[num]
+    end
+
+
+end
+school = School.new("Yukon Highschool")
+
